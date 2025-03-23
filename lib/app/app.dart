ed3991d96bdedd80af0fc360da48a82a51473085
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-03-23 12:35:50 +0600

    folder structure

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class EducationApp extends StatelessWidget {
+  const EducationApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(),
+    );
+  }
+}
+
+
+
+
+
+
+
